fe3a161bf7c796442ddc54ca8dc99df0f37c723a
Ameya Varma <varma.ameya@gmail.com>
2015-10-04 12:11:04 -0700

    add 2 numbers

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+
+        TextBox3.Text = a + b
+
+
+    End Sub
+End Class
